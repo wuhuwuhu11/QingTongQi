831c43ba6ce6554885c66102933d1945d0b2b5f4
wuhu <1256258358@qq.com>
2022-06-21 22:39:49 +0800

    6.21

---
+++ leecode/两数相加2.java
@@ -0,0 +1,15 @@
+package 练习练习.leecode;
+
+public class 两数相加2 {
+    public class ListNode{
+        int val;
+        ListNode next;
+
+        public ListNode(int val) {
+            this.val = val;
+        }
+    }
+//    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+//
+//    }
+}
--- 树/BM36.java
+++ 树/BM36.java
@@ -1,7 +1,8 @@
 package 练习练习.树;
+
 //BM36 判断是不是平衡二叉树
 public class BM36 {
-    public class TreeNode{
+    public class TreeNode {
         int val;
         TreeNode left;
         TreeNode right;
@@ -10,17 +11,19 @@ public class BM36 {
             this.val = val;
         }
     }
-    public static int deep(TreeNode head){
-        if (head==null) return 0;
+
+    public static int deep(TreeNode head) {
+        if (head == null) return 0;
         int left = deep(head.left);
-        int right= deep(head.right);
-        return left>right?left+1:right+1;
+        int right = deep(head.right);
+        return left > right ? left + 1 : right + 1;
     }
+
     public boolean IsBalanced_Solution(TreeNode root) {
-        if (root==null) return true;
+        if (root == null) return true;
         int left = deep(root.left);
         int right = deep(root.right);
-        if (left-right>1||right-left>1) return  false;
-        return IsBalanced_Solution(root.left)&&IsBalanced_Solution(root.right);
+        if (left - right > 1 || right - left > 1) return false;
+        return IsBalanced_Solution(root.left) && IsBalanced_Solution(root.right);
     }
 }
